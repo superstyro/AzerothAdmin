6bbc4d6ec4e1d4fe3070005f1d3afdb9b6c0369d
Tylerlee Burhans <superstyro@gmail.com>
2019-10-18 09:43:20 -0700

    Licence Update 2019

--- Frames/MangFrames_SectionMain.lua
+++ Frames/MangFrames_SectionMain.lua
@@ -1,8 +1,9 @@
 ﻿-------------------------------------------------------------------------------------------------------------
 --
--- MangAdmin Version 1.0
+-- AzerothAdmin Version 3.x
+-- AzerothAdmin is a derivative of TrinityAdmin/MangAdmin.
 --
--- Copyright (C) 2018 Free Software Foundation, Inc.
+-- Copyright (C) 2019 Free Software Foundation, Inc.
 -- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 -- This is free software: you are free to change and redistribute it.
 -- There is NO WARRANTY, to the extent permitted by law.
@@ -11,10 +12,7 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
--- Official Forums: http://www.manground.org/forum/
--- GoogleCode Website: http://code.google.com/p/mangadmin/
--- Subversion Repository: http://mangadmin.googlecode.com/svn/
---
+-- Repository: https://gitlab.superstyro.com/superstyro/azerothadmin
 -------------------------------------------------------------------------------------------------------------
 
 -- Initializing dynamic frames with LUA and FrameLib

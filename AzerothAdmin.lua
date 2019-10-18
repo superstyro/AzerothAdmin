6d69282b8d2aa61a4a7004484caf5051c42646da
Tylerlee Burhans <superstyro@gmail.com>
2019-10-18 09:45:03 -0700

    Rename file and update file name relation

--- AzerothAdmin.lua
+++ AzerothAdmin.lua
@@ -30,9 +30,9 @@ fID = 0
 gettingGOBinfo=0
 gettingGOBinfoinfo=0
 
-MAJOR_VERSION = "TrinityAdmin-3.3.5"
+MAJOR_VERSION = "AzerothAdmin-3.3.5"
 MINOR_VERSION = "$Revision: 055 $"
-ROOT_PATH     = "Interface\\AddOns\\TrinityAdmin\\"
+ROOT_PATH     = "Interface\\AddOns\\AzerothAdmin\\"
 local cont = ""
 if not AceLibrary then error(MAJOR_VERSION .. " requires AceLibrary") end
 if not AceLibrary:IsNewVersion(MAJOR_VERSION, MINOR_VERSION) then return end

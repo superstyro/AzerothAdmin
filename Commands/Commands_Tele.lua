def5011140bdb88405680bbcc6be8582e7bb730b
Tyler <superstyro@gmail.com>
2024-12-15 12:24:00 -0700

    Update repository links to point to GitHub

--- Commands/Commands_Tele.lua
+++ Commands/Commands_Tele.lua
@@ -12,7 +12,8 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
--- Repository: https://gitlab.superstyro.com/superstyro/azerothadmin
+-- Repository: https://github.com/superstyro/AzerothAdmin
+--
 -------------------------------------------------------------------------------------------------------------
 
 function TeleScrollUpdate()

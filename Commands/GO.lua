e7b4cb0dd9fdf8225ac99267744dc811cca69c1b
Tyler Burhans <superstyro@gmail.com>
2025-12-07 12:56:08 -0700

    Revert "fix gobject target command typo"
    This reverts commit 7417881d7a08e9d5bd8513780da783a9624bd89a.

--- Commands/GO.lua
+++ Commands/GO.lua
@@ -19,7 +19,7 @@
 function OBJGo()
     local player = UnitName("target") or UnitName("player")
     local obj = ma_Obj_guidbutton:GetText()
-    AzerothAdmin:ChatMsg(".goobject "..obj)
+    AzerothAdmin:ChatMsg(".go object "..obj)
     AzerothAdmin:LogAction("Go Object for player "..player..".")
 end
 
